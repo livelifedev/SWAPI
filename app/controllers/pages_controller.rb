c018c5be1c5e8cdfef297eef97a80fe9a7cd7385
John <johnrubio93@gmail.com>
2019-09-12 07:23:29 +1000

    added show view of films

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,11 @@ class PagesController < ApplicationController
         json = JSON.parse(response.body)
         @films = json["results"]
     end
+
+    def show
+        id = params["id"]
+        response = HTTParty.get("https://swapi.co/api/films/#{id}/")
+        json = JSON.parse(response.body)
+        @film = json
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
     get "/", to: "pages#home", as: "root"
+    get "/:id", to: "pages#show", as: "film"
 end
